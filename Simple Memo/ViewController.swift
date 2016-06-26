2a747fdf80dc66fad47f14ec5911a3d934fb40be
shimokp <oxo_inflove@yahoo.co.jp>
2016-06-26 12:39:46 +0900

    Initial Commit

---
+++ Simple Memo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Memo
+//
+//  Created by 下村一将 on 2016/06/26.
+//  Copyright © 2016年 kazu/juki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
