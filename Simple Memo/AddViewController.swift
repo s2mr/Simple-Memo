300608fd871de47bff857e408207a2f4a22f8550
jukey8 <jukey8@kindlybook.local>
2016-06-28 14:26:34 +0900

    コミット

--- Simple Memo/AddViewController.swift
+++ Simple Memo/AddViewController.swift
@@ -61,6 +61,7 @@ class AddViewController: UIViewController, UITextFieldDelegate {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    
 
 }
+    
+
